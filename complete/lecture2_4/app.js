71f5aadccf06479b746b63aab41086a88cb186fe
Nicholas Lever <nik.j.lever@gmail.com>
2020-06-04 15:55:04 +0100

    lecture2_4 complete

---
+++ complete/lecture2_4/app.js
@@ -0,0 +1,54 @@
+import * as THREE from '../../libs/three/three.module.js';
+import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+        
+		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
+		this.camera.position.set( 0, 0, 4 );
+        
+		this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0xaaaaaa );
+
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+		this.scene.add(ambient);
+        
+        const light = new THREE.DirectionalLight();
+        light.position.set( 0.2, 1, 1);
+        this.scene.add(light);
+			
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		container.appendChild( this.renderer.domElement );
+		
+        //Replace Box with Circle, Cone, Cylinder, Dodecahedron, Icosahedron, Octahedron, Plane, Sphere, Tetrahedron, Torus or TorusKnot
+        const geometry = new THREE.CircleBufferGeometry(1, 32, 0, Math.PI);
+        const material = new THREE.MeshStandardMaterial( { color: 0xFF0000 });
+
+        this.mesh = new THREE.Mesh( geometry, material );
+        
+        this.scene.add(this.mesh);
+        
+        const controls = new OrbitControls( this.camera, this.renderer.domElement );
+        
+        this.renderer.setAnimationLoop(this.render.bind(this));
+    
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    }
+    
+	render( ) {   
+        this.mesh.rotateY( 0.01 );
+        this.renderer.render( this.scene, this.camera );
+    }
+}
+
+export { App };
