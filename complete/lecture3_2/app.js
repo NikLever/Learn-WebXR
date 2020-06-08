737767b9fc20d0e93ecee119a38e6c882b4f3231
Nicholas Lever <nik@catalystpics.co.uk>
2020-06-08 13:08:17 +0100

    Controller example and custom controller

--- complete/lecture3_2/app.js
+++ complete/lecture3_2/app.js
@@ -1,6 +1,5 @@
 import * as THREE from '../../libs/three/three.module.js';
 import { VRButton } from './VRButton.js';
-import { XRControllerModelFactory } from '../../libs/three/jsm/XRControllerModelFactory.js';
 import { BoxLineGeometry } from '../../libs/three/jsm/BoxLineGeometry.js';
 import { Stats } from '../../libs/stats.module.js';
 import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
