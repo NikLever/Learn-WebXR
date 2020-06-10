bad65ca5c73af35f928d290b80b920d9b4e0ace9
Nicholas Lever <nik.j.lever@gmail.com>
2020-06-10 07:51:22 +0100

    Custom VRButton

---
+++ start/lecture3_3/app.js
@@ -0,0 +1,198 @@
+import * as THREE from '../../libs/three/three.module.js';
+import { VRButton } from './VRButton.js';
+import { XRControllerModelFactory } from '../../libs/three/jsm/XRControllerModelFactory.js';
+import { BoxLineGeometry } from '../../libs/three/jsm/BoxLineGeometry.js';
+import { Stats } from '../../libs/stats.module.js';
+import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+        
+        this.clock = new THREE.Clock();
+        
+		this.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 100 );
+		this.camera.position.set( 0, 1.6, 3 );
+        
+		this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0x505050 );
+
+		this.scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
+
+        const light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 1, 1, 1 ).normalize();
+		this.scene.add( light );
+			
+		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        
+		container.appendChild( this.renderer.domElement );
+        
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+        this.controls.target.set(0, 1.6, 0);
+        this.controls.update();
+        
+        this.stats = new Stats();
+        
+        this.raycaster = new THREE.Raycaster();
+        this.workingMatrix = new THREE.Matrix4();
+        this.workingVector = new THREE.Vector3();
+        
+        this.initScene();
+        this.setupVR();
+        
+        window.addEventListener('resize', this.resize.bind(this) );
+        
+        this.renderer.setAnimationLoop( this.render.bind(this) );
+	}	
+    
+    random( min, max ){
+        return Math.random() * (max-min) + min;
+    }
+    
+    initScene(){
+        this.radius = 0.08;
+        
+        this.room = new THREE.LineSegments(
+					new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ),
+					new THREE.LineBasicMaterial( { color: 0x808080 } )
+				);
+        this.room.geometry.translate( 0, 3, 0 );
+        this.scene.add( this.room );
+        
+        const geometry = new THREE.IcosahedronBufferGeometry( this.radius, 2 );
+
+        for ( let i = 0; i < 200; i ++ ) {
+
+            const object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+
+            object.position.x = this.random( -2, 2 );
+            object.position.y = this.random( -2, 2 );
+            object.position.z = this.random( -2, 2 );
+
+            this.room.add( object );
+
+        }
+        
+        this.highlight = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.BackSide } ) );
+        this.highlight.scale.set(1.2, 1.2, 1.2);
+    }
+    
+    setupVR(){
+        this.renderer.xr.enabled = true;
+        
+        const button = new VRButton( this.renderer );
+        
+        const self = this;
+        
+        function onSelectStart() {
+            
+            this.userData.selectPressed = true;
+        }
+
+        function onSelectEnd() {
+
+            this.children[0].scale.z = 0;
+            self.highlight.visible = false;
+            this.userData.selectPressed = false;
+            
+        }
+        
+        this.controller = this.renderer.xr.getController( 0 );
+        this.controller.addEventListener( 'selectstart', onSelectStart );
+        this.controller.addEventListener( 'selectend', onSelectEnd );
+        this.controller.addEventListener( 'connected', function ( event ) {
+
+            const mesh = self.buildController.call(self, event.data );
+            mesh.scale.z = 0;
+            this.add( mesh );
+
+        } );
+        this.controller.addEventListener( 'disconnected', function () {
+
+            this.remove( this.children[ 0 ] );
+            self.controller = null;
+            self.controllerGrip = null;
+
+        } );
+        this.scene.add( this.controller );
+
+        // The XRControllerModelFactory will automatically fetch controller models
+        // that match what the user is holding as closely as possible. The models
+        // should be attached to the object returned from getControllerGrip in
+        // order to match the orientation of the held device.
+
+        const controllerModelFactory = new XRControllerModelFactory();
+
+        this.controllerGrip = this.renderer.xr.getControllerGrip( 0 );
+        this.controllerGrip.add( controllerModelFactory.createControllerModel( this.controllerGrip ) );
+        this.scene.add( this.controllerGrip );
+        
+        this.scene.add(this.highlight);
+
+    }
+    
+    buildController( data ) {
+        let geometry, material;
+        
+        switch ( data.targetRayMode ) {
+            
+            case 'tracked-pointer':
+
+                geometry = new THREE.BufferGeometry();
+                geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 1 ], 3 ) );
+                geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( [ 0.5, 0.5, 0.5, 0, 0, 0 ], 3 ) );
+
+                material = new THREE.LineBasicMaterial( { vertexColors: true, blending: THREE.AdditiveBlending } );
+
+                return new THREE.Line( geometry, material );
+
+            case 'gaze':
+
+                geometry = new THREE.RingBufferGeometry( 0.02, 0.04, 32 ).translate( 0, 0, - 1 );
+                material = new THREE.MeshBasicMaterial( { opacity: 0.5, transparent: true } );
+                return new THREE.Mesh( geometry, material );
+
+        }
+
+    }
+    
+    handleController( controller ){
+        if (controller.userData.selectPressed ){
+            controller.children[0].scale.z = 10;
+
+            this.workingMatrix.identity().extractRotation( controller.matrixWorld );
+
+            this.raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
+            this.raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( this.workingMatrix );
+
+            const intersects = this.raycaster.intersectObjects( this.room.children );
+
+            if (intersects.length>0){
+                intersects[0].object.add(this.highlight);
+                this.highlight.visible = true;
+                controller.children[0].scale.z = intersects[0].distance;
+            }else{
+                this.highlight.visible = false;
+            }
+        }
+    }
+    
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    }
+    
+	render( ) {   
+        this.stats.update();
+        if (this.controller ) this.handleController( this.controller );
+        this.renderer.render( this.scene, this.camera );
+    }
+}
+
+export { App };
