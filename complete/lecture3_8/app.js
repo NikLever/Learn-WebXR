1c3f4f92a1cd08c2439a6b71965dd312ed5b7b9f
Nicholas Lever <nik.j.lever@gmail.com>
2020-06-12 07:38:31 +0100

    Completed VR physics example 3.8

--- complete/lecture3_8/app.js
+++ complete/lecture3_8/app.js
@@ -3,6 +3,7 @@ import { VRButton } from '../../libs/VRButton.js';
 import { XRControllerModelFactory } from '../../libs/three/jsm/XRControllerModelFactory.js';
 import { Stats } from '../../libs/stats.module.js';
 import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+import { CannonHelper } from '../../libs/CannonHelper.js';
 
 class App{
 	constructor(){
@@ -12,8 +13,8 @@ class App{
         this.clock = new THREE.Clock();
         
 		this.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 100 );
-		this.camera.position.set( 0, 1.6, 3 );
-        this.camera.lookAt( 0, 0.75, 0 );
+		this.camera.position.set( 0, 1.6, 0 );
+        this.camera.lookAt( 0, 0, -2 );
         
 		this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color( 0x505050 );
@@ -39,7 +40,7 @@ class App{
 		container.appendChild( this.renderer.domElement );
         
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-        this.controls.target.set(0, 3.5, 0);
+        this.controls.target.set(0, 0, -3);
         this.controls.update();
         
         this.stats = new Stats();
@@ -47,26 +48,25 @@ class App{
         this.initScene();
         this.setupVR();
         
+        this.renderer.setAnimationLoop( this.render.bind(this) );
+        
         this.raycaster = new THREE.Raycaster();
+        this.workingMatrix = new THREE.Matrix4();
+        this.origin = new THREE.Vector3();
         
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
     initScene(){
-      //Create a plane for following mouse movement
-      const geometry1 = new THREE.PlaneGeometry(100, 100);
-      const material1 = new THREE.MeshBasicMaterial({ visible: false, side: THREE.DoubleSide });
-      gplane = new THREE.Mesh( geometry1, material1 );
-      scene.add(gplane);
+        
+        //Create a marker to indicate where the joint is
+        const geometry = new THREE.SphereBufferGeometry( 0.1, 8, 8 );
+        const material = new THREE.MeshStandardMaterial({ color: 0xaa0000 });
+        this.marker = new THREE.Mesh( geometry, material );
+        this.marker.visible = false;
+        this.scene.add(this.marker);
 
-      //Create a marker to indicate where the joint is
-      const geometry2 = new THREE.SphereBufferGeometry( 0.1, 8, 8 );
-      const material2 = new THREE.MeshStandardMaterial({ color: 0xaa0000 });
-      marker = new THREE.Mesh( geometry2, material2 );
-      marker.visible = false;
-      scene.add(marker);
-
-      this.initPhysics();
+        this.initPhysics();
     }
     
     initPhysics(){
@@ -75,32 +75,169 @@ class App{
         this.dt = 1.0/60.0;
 	    this.damping = 0.01;
 		
-        world.broadphase = new CANNON.NaiveBroadphase();
-        world.gravity.set(0, -10, 0);
+        this.world.broadphase = new CANNON.NaiveBroadphase();
+        this.world.gravity.set(0, -10, 0);
   
-        this.helper = new CannonHelper( scene, world );
+        this.helper = new CannonHelper( this.scene, this.world );
 		
         const groundShape = new CANNON.Plane();
-        groundMaterial = new CANNON.Material();
+        const groundMaterial = new CANNON.Material();
         const groundBody = new CANNON.Body({ mass: 0, material: groundMaterial });
         groundBody.quaternion.setFromAxisAngle( new CANNON.Vec3(1,0,0), -Math.PI/2);
         groundBody.addShape(groundShape);
-        world.add(groundBody);
-        helper.addVisual(groundBody, 0xffaa00);
+        this.world.add(groundBody);
+        this.helper.addVisual(groundBody, 0xffaa00);
 
         // Joint body
         const shape = new CANNON.Sphere(0.1);
-        jointBody = new CANNON.Body({ mass: 0 });
-        jointBody.addShape(shape);
-        jointBody.collisionFilterGroup = 0;
-        jointBody.collisionFilterMask = 0;
-        world.add(jointBody);
+        this.jointBody = new CANNON.Body({ mass: 0 });
+        this.jointBody.addShape(shape);
+        this.jointBody.collisionFilterGroup = 0;
+        this.jointBody.collisionFilterMask = 0;
+        this.world.add(this.jointBody);
 
-        box = this.addBody();
-    }    
+        this.box = this.addBody();
+    }  
+    
+    addBody(box=true){
+        let shape;
+        if (!box){
+            shape = new CANNON.Sphere(0.5);
+        }else{
+            shape = new CANNON.Box(new CANNON.Vec3(0.5,0.5,0.5));
+        }
+        const material = new CANNON.Material();
+        const body = new CANNON.Body({ mass: 5, material: material });
+        body.addShape(shape);
+
+        body.position.set(0, 1, -3);
+        body.linearDamping = this.damping;
+        this.world.add(body);
+
+        this.helper.addVisual(body);
+
+        return body;
+    }
     
     setupVR(){
+        this.renderer.xr.enabled = true;
         
+        const button = new VRButton( this.renderer );
+        
+        const self = this;
+        
+        function onSelectStart() {
+            
+            this.userData.selectPressed = true;
+            if (this.userData.selected){
+                self.addConstraint( self.marker.getWorldPosition( self.origin ), self.box );
+                self.controller.attach( self.marker );
+            }
+        }
+
+        function onSelectEnd() {
+
+            this.userData.selectPressed = false;
+            const constraint = self.controller.userData.constraint;
+            if (constraint){
+                self.world.removeConstraint(constraint);
+                self.controller.userData.constraint = undefined;
+            }
+            
+        }
+        
+        this.controller = this.renderer.xr.getController( 0 );
+        this.controller.addEventListener( 'selectstart', onSelectStart );
+        this.controller.addEventListener( 'selectend', onSelectEnd );
+        this.controller.addEventListener( 'connected', function ( event ) {
+
+            const mesh = self.buildController.call(self, event.data );
+            mesh.scale.z = 0;
+            this.add( mesh );
+
+        } );
+        this.controller.addEventListener( 'disconnected', function () {
+
+            this.remove( this.children[ 0 ] );
+            self.controller = null;
+            self.controllerGrip = null;
+
+        } );
+        this.scene.add( this.controller );
+
+        const controllerModelFactory = new XRControllerModelFactory();
+
+        this.controllerGrip = this.renderer.xr.getControllerGrip( 0 );
+        this.controllerGrip.add( controllerModelFactory.createControllerModel( this.controllerGrip ) );
+        this.scene.add( this.controllerGrip );
+    }
+    
+
+    addConstraint(pos, body){
+        const pivot = pos.clone();
+        body.threemesh.worldToLocal(pivot);
+        
+        this.jointBody.position.copy(pos);
+ 
+        const constraint = new CANNON.PointToPointConstraint(body, pivot, this.jointBody, new CANNON.Vec3(0,0,0));
+
+        this.world.addConstraint(constraint);
+        
+        this.controller.userData.constraint = constraint;
+    }
+    
+    buildController( data ) {
+        let geometry, material;
+        
+        switch ( data.targetRayMode ) {
+            
+            case 'tracked-pointer':
+
+                geometry = new THREE.BufferGeometry();
+                geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0, 0, 0, - 1 ], 3 ) );
+                geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( [ 0.5, 0.5, 0.5, 0, 0, 0 ], 3 ) );
+
+                material = new THREE.LineBasicMaterial( { vertexColors: true, blending: THREE.AdditiveBlending } );
+
+                return new THREE.Line( geometry, material );
+
+            case 'gaze':
+
+                geometry = new THREE.RingBufferGeometry( 0.02, 0.04, 32 ).translate( 0, 0, - 1 );
+                material = new THREE.MeshBasicMaterial( { opacity: 0.5, transparent: true } );
+                return new THREE.Mesh( geometry, material );
+
+        }
+
+    }
+    
+    handleController( controller ){
+        if (!controller.userData.selectPressed){
+            controller.children[0].scale.z = 10;
+
+            this.workingMatrix.identity().extractRotation( controller.matrixWorld );
+
+            this.raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
+            this.raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( this.workingMatrix );
+
+            const intersects = this.raycaster.intersectObject( this.box.threemesh.children[0] );
+
+            if (intersects.length>0){
+                this.marker.position.copy(intersects[0].point);
+                this.marker.visible = true;
+                controller.children[0].scale.z = intersects[0].distance;
+                controller.userData.selected = true;
+            }else{
+                this.marker.visible = false;
+                controller.userData.selected = false;
+            }
+        }else{
+            const constraint = controller.userData.constraint;
+            if (constraint){
+                this.jointBody.position.copy( this.marker.getWorldPosition( this.origin ) );
+                constraint.update(); 
+            }
+        }
     }
     
     resize(){
@@ -111,133 +248,11 @@ class App{
     
 	render( ) {   
         this.stats.update();
+        if (this.renderer.xr.isPresenting) this.handleController( this.controller );
+        this.world.step(this.dt);
+        this.helper.update( );
         this.renderer.render( this.scene, this.camera );
     }
 }
 
 export { App };
-
-/*
-var scene, camera, renderer, controls, world, helper, dt, damping, marker, jointBody, raycaster, mouse, box;
-
-function initPhysics(){
-	world = new CANNON.World();
-		
-  dt = 1.0/60.0;
-	damping = 0.01;
-		
-	world.broadphase = new CANNON.NaiveBroadphase();
-	world.gravity.set(0, -10, 0);
-  
-  helper = new CannonHelper( scene, world );
-		
-	const groundShape = new CANNON.Plane();
-  groundMaterial = new CANNON.Material();
-	const groundBody = new CANNON.Body({ mass: 0, material: groundMaterial });
-	groundBody.quaternion.setFromAxisAngle( new CANNON.Vec3(1,0,0), -Math.PI/2);
-	groundBody.addShape(groundShape);
-	world.add(groundBody);
-  helper.addVisual(groundBody, 0xffaa00);
-  
-  // Joint body
-  const shape = new CANNON.Sphere(0.1);
-  jointBody = new CANNON.Body({ mass: 0 });
-  jointBody.addShape(shape);
-  jointBody.collisionFilterGroup = 0;
-  jointBody.collisionFilterMask = 0;
-  world.add(jointBody);
-  
-  box = addBody();
-}
-
-function addBody(box=true){
-  let shape;
-  if (!box){
-	  shape = new CANNON.Sphere(0.5);
-  }else{
-	  shape = new CANNON.Box(new CANNON.Vec3(0.5,0.5,0.5));
-  }
-  const material = new CANNON.Material();
-	const body = new CANNON.Body({ mass: 5, material: material });
-  body.addShape(shape);
-  	
-  body.position.set(0, 1, 0);
-	body.linearDamping = damping;
-	world.add(body);
-        
-  helper.addVisual(body);
-  
-  return body;
-}
-
-function onMouseMove( event ) {
-
-	// calculate mouse position in normalized device coordinates
-	// (-1 to +1) for both components
-
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-//console.log( `${mouse.x.toFixed(1)}, ${mouse.y.toFixed(1)}`);
-  
-  if (mouse.constraint){
-    raycaster.setFromCamera( mouse, camera );
-    const intersects = raycaster.intersectObject( gplane );
-    console.log( "constraint:" + intersects.length );
-    if (intersects.length>0){
-      marker.position.copy( intersects[0].point );
-      jointBody.position.copy( intersects[0].point );
-      mouse.constraint.update(); 
-    }
-  }
-}
-
-function onMouseDown( event ){
-  // Find mesh from a ray
-  raycaster.setFromCamera( mouse, camera );
-  const intersects = raycaster.intersectObject( box.threemesh, true );
-  if (intersects.length>0){
-    marker.visible = true;
-    marker.position.copy( intersects[0].point );
-    let object = intersects[0].object;
-    mouse.down = true;
-    //Align movement plane to intersection
-    gplane.position.copy(intersects[0].point);
-    gplane.lookAt(camera.position);  
-
-    addMouseConstraint(intersects[0].point, box);
-  }
-}
-
-function addMouseConstraint(pos, body){
-  const pivot = pos.clone();
-  body.threemesh.worldToLocal(pivot);
-
-  jointBody.position.copy(pos);
- 
-  mouse.constraint = new CANNON.PointToPointConstraint(body, pivot, jointBody, new CANNON.Vec3(0,0,0));
-
-  world.addConstraint(mouse.constraint);
-}
-
-function onMouseUp(){
-  marker.visible = false;
-  mouse.down = false;
-  if (mouse.constraint){
-    world.removeConstraint(mouse.constraint);
-    mouse.constraint = false;
-  }
-}
-
-function onWindowResize( event ) {
-  camera.aspect = window.innerWidth/window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-function update() {
-  requestAnimationFrame( update );
-  world.step(dt);
-  helper.update( );
-  renderer.render( scene, camera );
-}
-*/
---
+++ libs/CannonHelper.js
@@ -0,0 +1,266 @@
+import * as THREE from './three/three.module.js';
+
+class CannonHelper{
+    constructor(scene, world){
+        this.scene = scene;
+        this.world = world;
+    }
+    
+    addLights(renderer){
+        renderer.shadowMap.enabled = true;
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+
+        // LIGHTS
+        const ambient = new THREE.AmbientLight( 0x888888 );
+        this.scene.add( ambient );
+
+        const light = new THREE.DirectionalLight( 0xdddddd );
+        light.position.set( 3, 10, 4 );
+        light.target.position.set( 0, 0, 0 );
+
+        light.castShadow = true;
+
+        const lightSize = 10;
+        light.shadow.camera.near = 1;
+        light.shadow.camera.far = 50;
+        light.shadow.camera.left = light.shadow.camera.bottom = -lightSize;
+        light.shadow.camera.right = light.shadow.camera.top = lightSize;
+
+        light.shadow.mapSize.width = 1024;
+        light.shadow.mapSize.height = 1024;
+
+        this.sun = light;
+        this.scene.add(light);    
+    }
+    
+    createCannonTrimesh(geometry){
+		if (!geometry.isBufferGeometry) return null;
+		
+		const posAttr = geometry.attributes.position;
+		const vertices = geometry.attributes.position.array;
+		let indices = [];
+		for(let i=0; i<posAttr.count; i++){
+			indices.push(i);
+		}
+		
+		return new CANNON.Trimesh(vertices, indices);
+	}
+	
+	createCannonConvex(geometry){
+		if (!geometry.isBufferGeometry) return null;
+		
+		const posAttr = geometry.attributes.position;
+		const floats = geometry.attributes.position.array;
+		const vertices = [];
+		const faces = [];
+		let face = [];
+		let index = 0;
+		for(let i=0; i<posAttr.count; i+=3){
+			vertices.push( new CANNON.Vec3(floats[i], floats[i+1], floats[i+2]) );
+			face.push(index++);
+			if (face.length==3){
+				faces.push(face);
+				face = [];
+			}
+		}
+		
+		return new CANNON.ConvexPolyhedron(vertices, faces);
+	}
+    
+    addVisual(body, color=0x888888, name="mesh", castShadow=true, receiveShadow=true){
+		body.name = name;
+		if (this.defaultMaterial===undefined && color==0x888888) this.defaultMaterial = new THREE.MeshLambertMaterial({color:color});
+		const material = (color==0x888888) ? this.defaultMaterial : new THREE.MeshLambertMaterial({color:color});
+		if (this.settings===undefined){
+			this.settings = {
+				stepFrequency: 60,
+				quatNormalizeSkip: 2,
+				quatNormalizeFast: true,
+				gx: 0,
+				gy: 0,
+				gz: 0,
+				iterations: 3,
+				tolerance: 0.0001,
+				k: 1e6,
+				d: 3,
+				scene: 0,
+				paused: false,
+				rendermode: "solid",
+				constraints: false,
+				contacts: false,  // Contact points
+				cm2contact: false, // center of mass to contact points
+				normals: false, // contact normals
+				axes: false, // "local" frame axes
+				particleSize: 0.1,
+				shadows: false,
+				aabbs: false,
+				profiling: false,
+				maxSubSteps:3
+			}
+			this.particleGeo = new THREE.SphereGeometry( 1, 16, 8 );
+			this.particleMaterial = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
+		}
+		// What geometry should be used?
+		let mesh;
+		if(body instanceof CANNON.Body) mesh = this.shape2Mesh(body, castShadow, receiveShadow, material);
+
+		if(mesh) {
+			// Add body
+			body.threemesh = mesh;
+            mesh.castShadow = castShadow;
+            mesh.receiveShadow = receiveShadow;
+			this.scene.add(mesh);
+		}
+	}
+	
+	shape2Mesh(body, castShadow, receiveShadow, material=this.defaultMaterial){
+		const obj = new THREE.Object3D();
+		const game = this;
+		let index = 0;
+		
+		body.shapes.forEach (function(shape){
+			let mesh;
+			let geometry;
+			let v0, v1, v2;
+
+			switch(shape.type){
+
+			case CANNON.Shape.types.SPHERE:
+				const sphere_geometry = new THREE.SphereGeometry( shape.radius, 8, 8);
+				mesh = new THREE.Mesh( sphere_geometry, material );
+				break;
+
+			case CANNON.Shape.types.PARTICLE:
+				mesh = new THREE.Mesh( game.particleGeo, game.particleMaterial );
+				const s = this.settings;
+				mesh.scale.set(s.particleSize,s.particleSize,s.particleSize);
+				break;
+
+			case CANNON.Shape.types.PLANE:
+				geometry = new THREE.PlaneGeometry(10, 10, 4, 4);
+				mesh = new THREE.Object3D();
+				const submesh = new THREE.Object3D();
+				const ground = new THREE.Mesh( geometry, material );
+				ground.scale.set(100, 100, 100);
+				submesh.add(ground);
+
+				mesh.add(submesh);
+				break;
+
+			case CANNON.Shape.types.BOX:
+				const box_geometry = new THREE.BoxGeometry(  shape.halfExtents.x*2,
+															shape.halfExtents.y*2,
+															shape.halfExtents.z*2 );
+				mesh = new THREE.Mesh( box_geometry, material );
+				break;
+
+			case CANNON.Shape.types.CONVEXPOLYHEDRON:
+				const geo = new THREE.Geometry();
+
+				// Add vertices
+				shape.vertices.forEach(function(v){
+					geo.vertices.push(new THREE.Vector3(v.x, v.y, v.z));
+				});
+
+				shape.faces.forEach(function(face){
+					// add triangles
+					const a = face[0];
+					for (let j = 1; j < face.length - 1; j++) {
+						const b = face[j];
+						const c = face[j + 1];
+						geo.faces.push(new THREE.Face3(a, b, c));
+					}
+				});
+				geo.computeBoundingSphere();
+				geo.computeFaceNormals();
+				mesh = new THREE.Mesh( geo, material );
+				break;
+
+			case CANNON.Shape.types.HEIGHTFIELD:
+				geometry = new THREE.Geometry();
+
+				v0 = new CANNON.Vec3();
+				v1 = new CANNON.Vec3();
+				v2 = new CANNON.Vec3();
+				for (let xi = 0; xi < shape.data.length - 1; xi++) {
+					for (let yi = 0; yi < shape.data[xi].length - 1; yi++) {
+						for (let k = 0; k < 2; k++) {
+							shape.getConvexTrianglePillar(xi, yi, k===0);
+							v0.copy(shape.pillarConvex.vertices[0]);
+							v1.copy(shape.pillarConvex.vertices[1]);
+							v2.copy(shape.pillarConvex.vertices[2]);
+							v0.vadd(shape.pillarOffset, v0);
+							v1.vadd(shape.pillarOffset, v1);
+							v2.vadd(shape.pillarOffset, v2);
+							geometry.vertices.push(
+								new THREE.Vector3(v0.x, v0.y, v0.z),
+								new THREE.Vector3(v1.x, v1.y, v1.z),
+								new THREE.Vector3(v2.x, v2.y, v2.z)
+							);
+							var i = geometry.vertices.length - 3;
+							geometry.faces.push(new THREE.Face3(i, i+1, i+2));
+						}
+					}
+				}
+				geometry.computeBoundingSphere();
+				geometry.computeFaceNormals();
+				mesh = new THREE.Mesh(geometry, material);
+				break;
+
+			case CANNON.Shape.types.TRIMESH:
+				geometry = new THREE.Geometry();
+
+				v0 = new CANNON.Vec3();
+				v1 = new CANNON.Vec3();
+				v2 = new CANNON.Vec3();
+				for (let i = 0; i < shape.indices.length / 3; i++) {
+					shape.getTriangleVertices(i, v0, v1, v2);
+					geometry.vertices.push(
+						new THREE.Vector3(v0.x, v0.y, v0.z),
+						new THREE.Vector3(v1.x, v1.y, v1.z),
+						new THREE.Vector3(v2.x, v2.y, v2.z)
+					);
+					var j = geometry.vertices.length - 3;
+					geometry.faces.push(new THREE.Face3(j, j+1, j+2));
+				}
+				geometry.computeBoundingSphere();
+				geometry.computeFaceNormals();
+				mesh = new THREE.Mesh(geometry, MutationRecordaterial);
+				break;
+
+			default:
+				throw "Visual type not recognized: "+shape.type;
+			}
+
+			mesh.receiveShadow = receiveShadow;
+			mesh.castShadow = castShadow;
+            
+            mesh.traverse( function(child){
+                if (child.isMesh){
+                    child.castShadow = castShadow;
+					child.receiveShadow = receiveShadow;
+                }
+            });
+
+			var o = body.shapeOffsets[index];
+			var q = body.shapeOrientations[index++];
+			mesh.position.set(o.x, o.y, o.z);
+			mesh.quaternion.set(q.x, q.y, q.z, q.w);
+
+			obj.add(mesh);
+		});
+
+		return obj;
+	}
+    
+    update(){
+        this.world.bodies.forEach( function(body){
+            if ( body.threemesh !== undefined){
+                body.threemesh.position.copy(body.position);
+                body.threemesh.quaternion.copy(body.quaternion);
+            }
+        });
+    }
+}
+
+export { CannonHelper };
