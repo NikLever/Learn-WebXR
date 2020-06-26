704a8d44b57656a72b949d264ba05aa8634150ae
Nicholas Lever <nik@catalystpics.co.uk>
2020-06-26 10:54:29 +0100

    Organising 2_2 screencast

---
+++ start/lecture2_2/app.js
@@ -0,0 +1,21 @@
+import * as THREE from '../../libs/three/three.module.js';
+import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+    
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    resize(){
+        
+    }
+    
+	render( ) {   
+        
+    }
+}
+
+export { App };
