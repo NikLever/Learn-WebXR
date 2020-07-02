699c0fbb2c4a650df84f5f8979838f2daa41a81b
Nicholas Lever <nik@catalystpics.co.uk>
2020-07-02 09:20:04 +0100

    Small fix to 2.5

--- start/lecture2_5/app.js
+++ start/lecture2_5/app.js
@@ -26,7 +26,7 @@ class App{
 		
         const geometry = new THREE.TorusKnotBufferGeometry( 0.8, 0.3, 120, 16 ); 
         
-        const material = new THREE.MeshStandardMaterial( { color: 0x00FF00, metalness: 0.2, roughness: 0.8 });
+        const material = new THREE.MeshBasicMaterial( { color: 0xFF0000 });
 
         this.mesh = new THREE.Mesh( geometry, material );
         
