d0f14179bdce75427ce99ed8c441af4989f97a9e
Nicholas Lever <nik.j.lever@gmail.com>
2020-08-18 17:32:08 +0100

    Working on 8.2

--- libs/Interactable.js
+++ libs/Interactable.js
@@ -23,25 +23,35 @@ class Interactable{
         
         const tweens = this.options.tweens;
         tweens.forEach( tween => {
-            tween.target[tween.channel] = (tween.end - tween.start) * delta + tween.start;
+            tween.target[tween.channel] = (tween.end - tween.start) * value + tween.start;
         });
+    }
+    
+    get delta(){
+        //Only applies to tween interactable
+        if (!this.options || !this.options.mode=='tweens') return;
+        
+        const tween = this.options.tweens[0];
+        const currentValue = tween.target[tween.channel];
+        
+        return (currentValue - tween.start)/(tween.end - tween.start);
     }
     
     onComplete(){
         this.tweens = [];
     }
     
-    set play(value){
-        //Only applies to tween interactable
-        if (!this.options || !this.options.mode=='tweens') return;
+    play(value){
+        //Only applies to tween interactable and it cannot be currently playing
+        if (!this.options || !this.options.mode=='tweens' || this.tweens.length>0) return;
+        
+        if (value===undefined){
+            value = ( this.delta<0.1 ) ? 1 : -1;
+        }
         
         const tweens = this.options.tweens;
             
         switch(value){
-            case 0:
-                //stop
-                this.tweens = [];
-                break;
             case 1:
                 tweens.forEach( tween => {
                     const target = tween.target;
