e705287b114f7481ace11f58a93b0ef08848bb55
Nicholas Lever <nik.j.lever@gmail.com>
2020-09-12 08:40:26 +0100

    Writing 8.3

---
+++ complete/lecture7_3/ARButton.js
@@ -0,0 +1,195 @@
+/**
+ * @author mrdoob / http://mrdoob.com
+ * @author Mugen87 / https://github.com/Mugen87
+ * @author NikLever / http://niklever.com
+ */
+
+class ARButton{
+
+	constructor( renderer, options ) {
+        this.renderer = renderer;
+        
+        if (options !== undefined){
+            this.onSessionStart = options.onSessionStart;
+            this.onSessionEnd = options.onSessionEnd;
+            this.sessionInit = options.sessionInit;
+        }
+        
+        if ( 'xr' in navigator ) {
+
+			const button = document.createElement( 'button' );
+			button.style.display = 'none';
+            button.style.height = '40px';
+
+			navigator.xr.isSessionSupported( 'immersive-ar' ).then( ( supported ) => {
+
+				supported ? this.showStartAR( button ) : this.showARNotSupported( button );
+
+			} );
+            
+            document.body.appendChild( button );
+
+		} else {
+
+			const message = document.createElement( 'a' );
+
+			if ( window.isSecureContext === false ) {
+
+				message.href = document.location.href.replace( /^http:/, 'https:' );
+				message.innerHTML = 'WEBXR NEEDS HTTPS'; 
+
+			} else {
+
+				message.href = 'https://immersiveweb.dev/';
+				message.innerHTML = 'WEBXR NOT AVAILABLE';
+
+			}
+
+			message.style.left = '0px';
+			message.style.width = '100%';
+			message.style.textDecoration = 'none';
+
+			this.stylizeElement( message, false );
+            message.style.bottom = '0px';
+            message.style.opacity = '1';
+            
+            document.body.appendChild ( message );
+
+		}
+
+    }
+
+	showStartAR( button ) {
+
+        let currentSession = null;
+        const self = this;
+        
+        this.stylizeElement( button, true, 30, true );
+        
+        function onSessionStarted( session ) {
+
+            session.addEventListener( 'end', onSessionEnded );
+
+            self.renderer.xr.setReferenceSpaceType( 'local' );
+            self.renderer.xr.setSession( session );
+            self.stylizeElement( button, false, 12, true );
+            
+            button.textContent = 'STOP AR';
+
+            currentSession = session;
+            
+            if (self.onSessionStart !== undefined && self.onSessionStart !== null) self.onSessionStart();
+
+        }
+
+        function onSessionEnded( ) {
+
+            currentSession.removeEventListener( 'end', onSessionEnded );
+
+            self.stylizeElement( button, true, 12, true );
+            button.textContent = 'START AR';
+
+            currentSession = null;
+            
+            if (self.onSessionEnd !== undefined && self.onSessionEnd !== null) self.onSessionEnd();
+
+        }
+
+        //
+
+        button.style.display = '';
+        button.style.right = '20px';
+        button.style.width = '80px';
+        button.style.cursor = 'pointer';
+        button.innerHTML = '<img src="camera.svg" class="camera" />';
+        
+
+        button.onmouseenter = function () {
+            
+            button.style.fontSize = '12px'; 
+            button.textContent = (currentSession===null) ? 'START AR' : 'STOP AR';
+            button.style.opacity = '1.0';
+
+        };
+
+        button.onmouseleave = function () {
+            
+            button.style.fontSize = '30px'; 
+            button.innerHTML = '<img src="camera.svg"  class="camera" />';
+            button.style.opacity = '0.5';
+
+        };
+
+        button.onclick = function () {
+
+            if ( currentSession === null ) {
+
+                // WebXR's requestReferenceSpace only works if the corresponding feature
+                // was requested at session creation time. For simplicity, just ask for
+                // the interesting ones as optional features, but be aware that the
+                // requestReferenceSpace call will fail if it turns out to be unavailable.
+                // ('local' is always available for immersive sessions and doesn't need to
+                // be requested separately.)
+                
+                navigator.xr.requestSession( 'immersive-ar', self.sessionInit ).then( onSessionStarted );
+
+            } else {
+
+                currentSession.end();
+
+            }
+
+        };
+
+    }
+
+    disableButton(button) {
+
+        button.style.cursor = 'auto';
+        button.style.opacity = '0.5';
+        
+        button.onmouseenter = null;
+        button.onmouseleave = null;
+
+        button.onclick = null;
+
+    }
+
+    showARNotSupported( button ) {
+        this.stylizeElement( button, false );
+        
+        this.disableButton(button);
+
+        button.style.display = '';
+        button.style.width = '100%';
+        button.style.right = '0px';
+        button.style.bottom = '0px';
+        button.style.border = '';
+        button.style.opacity = '1';
+        button.style.fontSize = '13px';
+        button.textContent = 'AR NOT SUPPORTED';
+
+    }
+
+    stylizeElement( element, active = true, fontSize = 13, ignorePadding = false ) {
+
+        element.style.position = 'absolute';
+        element.style.bottom = '20px';
+        if (!ignorePadding) element.style.padding = '12px 6px';
+        element.style.border = '1px solid #fff';
+        element.style.borderRadius = '4px';
+        element.style.background = (active) ? 'rgba(20,150,80,1)' : 'rgba(180,20,20,1)';
+        element.style.color = '#fff';
+        element.style.font = `normal ${fontSize}px sans-serif`;
+        element.style.textAlign = 'center';
+        element.style.opacity = '0.5';
+        element.style.outline = 'none';
+        element.style.zIndex = '999';
+
+    }
+
+		
+
+};
+
+export { ARButton };
