db374c9a7226b886df9c001c0a1f7bb6c7568b91
gabrieleru <gabriele.rubino.ing@gmail.com>
2021-08-09 12:06:58 +0200

    Adding code_1

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -5,6 +5,18 @@ class App{
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
+		this.camera=new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight,0.1, 100);
+		this.camera.position.set(0,0,4);
+
+		this.scene = new THREE.Scene();
+		this.scene.background=new THREE.Color (0xaaaaaa);
+		this.renderer= new THREE.WebGLRenderer({antialias:true});
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize (window.innerWidth, window.innerHeight);
+		container.appendChild(this.renderer.domElement);
+
+		this.renderer.setAnimationLoop(this.render.bind(this));
+
     
         
         window.addEventListener('resize', this.resize.bind(this) );
@@ -15,6 +27,7 @@ class App{
     }
     
 	render( ) {  
+		this.renderer.render(this.scene, this.camera);
         
     }
 }
